aeca0ad53436a0d955a04203c52670e425c36f1c
Pranmoy <pranmoypatar1234@gmail.com>
2024-10-20 11:01:27 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -7,7 +8,18 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is runing at port: ${process.env.PORT}`)
+    })
+    app.on("error",(error)=>{
+        console.log("ERR:", error);
+        throw error;
+    })
+})
+.catch((err)=>{
+    console.log("connection falied: mongoDB", err)
+})
 
 
 
---
+++ src/utils/asynchandeler.js
@@ -0,0 +1,22 @@
+const asynchandeler = (requestHandeler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandeler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asynchandeler }
+
+// const asynchandeler = () => {}
+// const asynchandeler = (fn) => {() => {}}
+// const asynchandeler = (fn) => async () => {}
+
+// const asynchandeler = (fn) => async (req, res, next) => {
+//     try{
+//           await fn(req, res, next)
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
